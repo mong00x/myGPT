bfb16eaec5a6aeccd5ef201e81c446d0da1d49e6
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2023-06-11 22:32:14 +0930

    add myOpanaiApi django project

---
+++ server/my_opai_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyOpaiApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_opai_api'
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_opai_api'
+    'rest_api_framework'
+    
 ]
 
 MIDDLEWARE = [
