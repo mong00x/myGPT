4aad3d956fcd20e9d6de300f5e875f9bb1d8077c
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2023-06-22 16:19:47 +0930

    fixes

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -7,7 +7,7 @@ const Message = () => {
         <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence"/>
         <Box bg='blackAlpha.50' maxW='85%' p='4'  rounded='2xl' >
           <Text color='blackAlpha.800'>
-            Hi, Lets Chat!
+            Hi, Let's Chat!
           </Text>  
         </Box>
         
--- client/src/components/MessageField.jsx
+++ client/src/components/MessageField.jsx
@@ -4,22 +4,16 @@ import Message from './Message'
 
 const MessageField = () => {
   return (
-    <Stack  px='12' mb='4' height='100%' overflowY='auto' rounded='12' backgroundColor='#FFFBFE' >
+    <Stack  
+      px='12' 
+      mb='4' 
+      height='100%' 
+      overflowY='auto' 
+      rounded='12' 
+      backgroundColor='#FFFBFE' 
+    >
         <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        
         
     </Stack>
   )
