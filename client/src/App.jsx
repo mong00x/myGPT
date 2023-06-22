1124b65c0ad767046d97b84feea48378475777ee
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2023-06-23 00:29:47 +0930

    handle data in app.jsx instead of ChatInput, add messages and pass down to messagefield

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,42 @@ import Header from './components/Header'
 import MessageField from './components/MessageField'
 import ChatInput from './components/ChatInput'
 import { Box, Container,Flex } from '@chakra-ui/react'
+import {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (role, content) => {
+    // add a new message to the list
+    setMessages(prevMessages => [...prevMessages, { role, content }]);
+  };
+
+  const sendMessage = (message) => {
+    // When the user sends a message, add it and a placeholder for the bot's response
+    console.log(message);
+    addMessage("user", message);
+    addMessage("bot", "");  // Placeholder bot message
+    setIsLoading(true);
+
+    axios.post('http://localhost:8000/chat/', { prompt: message })
+      .then((response) => {
+        // Replace the placeholder bot message with the actual response
+        setMessages(prevMessages => {
+          const updatedMessages = [...prevMessages];
+          updatedMessages[updatedMessages.length - 1].content = response.data.response.content;
+          return updatedMessages;
+        });
+        setIsLoading(false);
+      }, (error) => {
+        console.log(error);
+      });
+  };
 
   return (
       
-    <Box fontFamily='roboto'>
+    <Box fontFamily='roboto' pt='8'>
       <div className="patterned-circle"></div>
       <Box>
         <Header />
@@ -19,8 +49,8 @@ function App() {
           blur='8px'
         >
           <Flex flexDir='column' justify='space-between' h='100%' >
-            <MessageField />
-            <ChatInput />
+          <MessageField messages={messages} isLoading={isLoading} />
+            <ChatInput sendMessage={sendMessage} isLoading={isLoading} />
           </Flex>
         </Container>
       </Box>
--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -1,9 +1,10 @@
-// import React from 'react'
-import { Flex, Button, Input  } from "@chakra-ui/react"
-import axios from 'axios'
+import { Flex, Button, Input } from "@chakra-ui/react"
+import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-const ChatInput = () => {
-  // axios post 
+const ChatInput = ({ sendMessage, isLoading }) => {
+  const [message, setMessage] = useState("");
+
   // debounce
   const debounce = (func, wait) => {
     let timeout;
@@ -17,59 +18,64 @@ const ChatInput = () => {
     };
   };
 
+  const debouncedSendMessage = debounce(sendMessage, 500);
 
-  const sendMessage = () => {
-    axios.post('http://localhost:8000/chat/', {
-      prompt: 'Hi'
-    })
-    .then((response) => {
-      console.log(response.data);
-    }, (error) => {
-      console.log(error);
-    });
-  }
-  
-
-
+  const handleSendMessage = (event) => {
+    event.preventDefault();
+    debouncedSendMessage(message);
+    setMessage("");
+  };
 
   return (
-    <Flex 
-      gap={4} 
-      rounded={8} 
-      mx='8' 
-      justify='center'
-      alignItems='center'
-      mb='8'
-    >
-      <Input 
-        placeholder="Type your message here" 
-        border='2px'  
-        borderColor='#dfd5ec33'
-        px='5' 
-        py='7' 
-        rounded='100'
-      />
-      <Button 
-        onClick={debounce(sendMessage, 1000)}
-        rounded='100' 
-        bg='none' 
-        w='14' 
-        h='14' 
-        p='3' 
-        _hover={{bg:'#66666622'}}
-        _active={{bg:'#66666644'}} 
+    <form onSubmit={handleSendMessage}> 
+      <Flex 
+        gap={4} 
+        rounded={8} 
+        mx='8' 
+        justify='center'
+        alignItems='center'
+        mb='8'
       >
-      <svg 
-        width="48" 
-        height="48"  
-        viewBox="0 0 24 24" 
-        xmlns="http://www.w3.org/2000/svg"><path d="m12.815 12.197-7.532 1.256a.5.5 0 0 0-.386.318L2.3 20.728c-.248.64.421 1.25 1.035.943l18-9a.75.75 0 0 0 0-1.342l-18-9c-.614-.307-1.283.304-1.035.943l2.598 6.957a.5.5 0 0 0 .386.319l7.532 1.255a.2.2 0 0 1 0 .394Z" 
-        fill="#6750a4"
-      />
-      </svg>
+        <Input 
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type your message here" 
+          border='2px'  
+          borderColor='#dfd5ec33'
+          px='5' 
+          py='7' 
+          rounded='100'
+        />
+        <Button 
+          type="submit"
+          rounded='100' 
+          bg='none' 
+          w='14' 
+          h='14' 
+          p='3' 
+          _hover={{bg:'#66666622'}}
+          _active={{bg:'#66666644'}} 
+          disabled={isLoading}
+        >
+          <svg 
+            width="48" 
+            height="48"  
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg"><path d="m12.815 12.197-7.532 1.256a.5.5 0 0 0-.386.318L2.3 20.728c-.248.64.421 1.25 1.035.943l18-9a.75.75 0 0 0 0-1.342l-18-9c-.614-.307-1.283.304-1.035.943l2.598 6.957a.5.5 0 0 0 .386.319l7.532 1.255a.2.2 0 0 1 0 .394Z" 
+            fill="#6750a4"
+          />
+          </svg>
         </Button>
-    </Flex>
+      </Flex>
+
+    </form>
+      
   )
+}
+
+ChatInput.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  sendMessage: PropTypes.func.isRequired,
 }
 
 export default ChatInput
