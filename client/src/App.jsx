0d0d2368423377c4985014e914eb9602f523c364
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2023-07-02 00:43:18 +0930

    update new api endpoint with domain name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     
     if (import.meta.env.MODE === 'production') {
       // production
-      axios.post('http://3.27.164.53:8000/chat/', { prompt: message })
+      axios.post('http://www.myopenaiapi.net/chat/', { prompt: message })
       .then((response) => {
         // Replace the placeholder bot message with the actual response
         setMessages(prevMessages => {
